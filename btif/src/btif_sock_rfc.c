codeaurora-unoffical/platform-external-bluetooth-bluedroid

commit 30b0a2d049e17d1c47050798080a3cfdff15614c

Bluetooth: Delay setting connectable mode.
This patch delays setting of Connectable mode until data profile
are registered.

Change-Id: Ifd43034ebe167aa308449cb6817801f18a1540cb
CRs-Fixed: 538672

btif/src/btif_sock_rfc.c
@@ -150,6 +150,7 @@ static void init_rfc_slots()
 bt_status_t btsock_rfc_init(int poll_thread_handle)
 {
     pth = poll_thread_handle;
+    btif_data_profile_register(0);
     init_rfc_slots();
     return BT_STATUS_SUCCESS;
 }
@@ -330,6 +331,9 @@ bt_status_t btsock_rfc_listen(const char* service_name, const uint8_t* service_u
     *sock_fd = -1;
     if(!is_init_done())
         return BT_STATUS_NOT_READY;
+
+    btif_data_profile_register(1);
+
     if(channel == RESERVED_SCN_FTP)
     {
         service_uuid = UUID_FTP;
@@ -520,6 +524,9 @@ static void cleanup_rfc_slot(rfc_slot_t* rs)
         close(rs->fd);
         rs->fd = -1;
     }
+
+    btif_data_profile_register(0);
+
     if(rs->app_fd != -1)
     {
         close(rs->app_fd);

btif/include/btif_common.h
@@ -175,6 +175,7 @@ tBTA_SERVICE_MASK btif_get_enabled_services_mask(void);
 bt_status_t btif_enable_service(tBTA_SERVICE_ID service_id);
 bt_status_t btif_disable_service(tBTA_SERVICE_ID service_id);
 int btif_is_enabled(void);
+void btif_data_profile_register(int value);
 
 /**
  * BTIF_Events

btif/src/btif_core.c
@@ -110,6 +110,8 @@ static btif_core_state_t btif_core_state = BTIF_CORE_STATE_DISABLED;
 
 static int btif_shutdown_pending = 0;
 static tBTA_SERVICE_MASK btif_enabled_services = 0;
+static int btif_data_profile_registered = 0;
+static int btif_pending_mode = BT_SCAN_MODE_NONE;
 
 /*
 * This variable should be set to 1, if the Bluedroid+BTIF libraries are to
@@ -1247,6 +1249,14 @@ bt_status_t btif_set_adapter_property(const bt_property_t *property)
 
                 BTIF_TRACE_EVENT1("set property scan mode : %x", mode);
 
+                if (!btif_data_profile_registered && mode != BT_SCAN_MODE_NONE)
+                {
+                    btif_pending_mode = mode;
+                    BTIF_TRACE_DEBUG0("btif_set_adapter_property: not setting connectable mode, "
+                        "as data profiles are not yet registered. Mode will be set when "
+                        "data profile(s) are registered");
+                    return BT_STATUS_SUCCESS;
+                }
                 BTA_DmSetVisibility(disc_mode, conn_mode, BTA_DM_IGNORE, BTA_DM_IGNORE);
 
                 storage_req_id = BTIF_CORE_STORAGE_ADAPTER_WRITE;
@@ -1472,4 +1482,26 @@ bt_status_t btif_disable_service(tBTA_SERVICE_ID service_id)
     }
 
     return BT_STATUS_SUCCESS;
+}
+
+void btif_data_profile_register(int value)
+{
+    bt_property_t property;
+    int val;
+
+    if (value == btif_data_profile_registered || btif_pending_mode == BT_SCAN_MODE_NONE)
+        return;
+
+    BTIF_TRACE_EVENT2("%s: Data profile registration = %d", __FUNCTION__, value);
+    btif_data_profile_registered = value;
+    if (btif_data_profile_registered)
+    {
+        property.type = BT_PROPERTY_ADAPTER_SCAN_MODE;
+        val = btif_pending_mode;
+        property.val = &val;;
+        property.len = (sizeof(int));
+        /* Reset pending mode to None */
+        btif_pending_mode == BT_SCAN_MODE_NONE;
+        btif_set_adapter_property(&property);
+    }
 }

commit d3075d59d762be910c7b2d18e38fc489cdf50497

Bluetooth: Reset data profile registration during socket cleanup
This patch fixes the regression caused by commit
30b0a2d049e17d1c47050798080a3cfdff15614c where data profile registration
was being reset during cleanup_rfc_slot() API which is called after
each successful data profile transfer which prevented changing viibility
mode after data transfer was complete. With this patch now data profile
registration is being moved to btsock_rfc_cleanup() API which is called
when BT is being turned off.

Change-Id: I6d8dee245ee9037b85022b536cc914e63db526e0
CRs-Fixed: 538672

btif/src/btif_sock_rfc.c
@@ -163,6 +163,7 @@ void btsock_rfc_cleanup()
 {
     int curr_pth = pth;
     pth = -1;
+    btif_data_profile_register(0);
     btsock_thread_exit(curr_pth);
     lock_slot(&slot_lock);
     int i;
@@ -642,8 +643,6 @@ static void cleanup_rfc_slot(rfc_slot_t* rs)
         close(rs->fd);
         rs->fd = -1;
     }
-
-    btif_data_profile_register(0);
 
     if(rs->app_fd != -1)
     {