2f051b79f53e73e3fee3a0fd18a92581c4105694
Amit Goel <agoel@codeaurora.org>
2014-05-02 17:39:59 -0700

    Bluetooth: Increase limit of bluetooth devices connectable simultaneously
    Limit of number of bluetooth devices that can connect at the same time is
    increased from 7 to 16.
    
    Change-Id: I69d3e26d21ae075107e039a0d9dba0324e773bfd
    CRs-fixed: 657232

--- include/bt_target.h
+++ include/bt_target.h
@@ -1470,7 +1470,11 @@ and USER_HW_DISABLE_API macros */
 #endif
 
 #ifndef GATT_MAX_APPS
+#ifndef MAX_ACL_CONNECTIONS
 #define GATT_MAX_APPS            10 /* note: 2 apps used internally GATT and GAP */
+#else
+#define GATT_MAX_APPS            MAX_ACL_CONNECTIONS
+#endif
 #endif
 
 #ifndef GATT_MAX_CL_PROFILES
@@ -1478,7 +1482,11 @@ and USER_HW_DISABLE_API macros */
 #endif
 
 #ifndef GATT_MAX_PHY_CHANNEL
+#ifndef MAX_ACL_CONNECTIONS
 #define GATT_MAX_PHY_CHANNEL        7
+#else
+#define GATT_MAX_PHY_CHANNEL        MAX_ACL_CONNECTIONS
+#endif
 #endif
 
 /* Used for conformance testing ONLY */
@@ -2887,7 +2895,11 @@ Range: Minimum 12000 (12 secs) on BR/EDR when supporting PBF.
 #endif
 
 #ifndef HID_HOST_MAX_DEVICES
+#ifndef MAX_ACL_CONNECTIONS
 #define HID_HOST_MAX_DEVICES        7
+#else
+#define HID_HOST_MAX_DEVICES        MAX_ACL_CONNECTIONS
+#endif
 #endif
 
 #ifndef HID_HOST_MTU
