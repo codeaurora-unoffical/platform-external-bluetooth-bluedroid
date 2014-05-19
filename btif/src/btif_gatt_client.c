f5f79d98c37dafaf0444b4734bb60d61e451dd88
Satish kumar sugasi <ssugas@codeaurora.org>
2014-05-19 10:57:01 -0700

    Bluetooth: Avoid Encryption after gatt server connection complete.
    This doesn't start encrtyption by default after connecting to a client.
    needed for certain test cases of BLE stack certification.
    
    Change-Id: I04dbe1daba32b416f59fed00989168507ce261fa
    CRs-Fixed: 652485

--- btif/src/btif_gatt_client.c
+++ btif/src/btif_gatt_client.c
@@ -440,7 +440,9 @@ static void btif_gattc_upstreams_evt(uint16_t event, char* p_param)
                 , p_data->open.status, p_data->open.client_if, &bda);
 
             if (p_data->open.status == BTA_GATT_OK)
+#if (!defined(BTA_SKIP_BLE_START_ENCRYPTION) || BTA_SKIP_BLE_START_ENCRYPTION == FALSE)
                 btif_gatt_check_encrypted_link(p_data->open.remote_bda);
+#endif
             break;
         }
 
