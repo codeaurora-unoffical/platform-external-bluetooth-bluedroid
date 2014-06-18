ad1bb4c9a2162c691ef5ca2555b31b91630d4af3
Nitin Arora <niarora@codeaurora.org>
2014-06-18 14:19:05 -0700

    Bluetooth: Change to prevent dev unpair caused by host disconn
    This patch includes changes to prevent unpairing the device
    in case a disconnection is initiated by the host, when an encryption
    is still in progress.
    
    CRs-Fixed: 694033
    Change-Id: I134fa0f1f7f574007cce4814ff8f677e747221ab

--- btif/src/btif_gatt_util.c
+++ btif/src/btif_gatt_util.c
@@ -271,7 +271,9 @@ static BOOLEAN btif_gatt_is_link_encrypted (BD_ADDR bd_addr)
 
 static void btif_gatt_set_encryption_cb (BD_ADDR bd_addr, tBTA_STATUS result)
 {
-    if (result == BTA_BUSY || result == BTA_BAD_RF || result == BTA_TIMEOUT)
+    ALOGI("%s result=%d", __FUNCTION__, result);
+    if (result == BTA_BUSY || result == BTA_BAD_RF || result == BTA_TIMEOUT ||
+         result == BTA_WRONG_MODE || result == BTA_HOST_DISCONN)
     {
         ALOGE("%s: callback called with status busy. No Action.", __FUNCTION__);
         return;
