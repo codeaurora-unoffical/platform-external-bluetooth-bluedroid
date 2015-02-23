CyanogenMod/android_external_bluetooth_bluedroid

Bluetooth: Fix to avoid crash duing dealloc gki buffer
Fix for segmentation fault caused during dealloc gki buffer when
gki is already shutdown during BT turn off.

Change-Id: I89092eda82e451ce7c8f56919210052bb55e349d
CRs-fixed: 728957

main/bte_main.c
@@ -104,6 +104,7 @@ static void bte_hci_disable(void);
 static void preload_start_wait_timer(void);
 static void preload_stop_wait_timer(void);
 
+BOOLEAN btif_is_shutdown(void);
 /*******************************************************************************
 **  Externs
 *******************************************************************************/
@@ -768,7 +769,14 @@ static char *alloc(int size)
 ******************************************************************************/
 static void dealloc(TRANSAC transac)
 {
-    GKI_freebuf(transac);
+    if (btif_is_shutdown() == FALSE)
+    {
+        GKI_freebuf(transac);
+    }
+    else
+    {
+        APPL_TRACE_WARNING("GKI is already shutdown ");
+    }
 }
 
 /******************************************************************************

codeaurora-unoffical/platform-external-bluetooth-bluedroid

main/bte_main.c
@@ -99,6 +99,7 @@ static void bte_send_preload_req(void);
 static void preload_start_wait_timer(void);
 static void preload_stop_wait_timer(void);
 
+BOOLEAN btif_is_shutdown(void);
 /*******************************************************************************
 **  Externs
 *******************************************************************************/
@@ -720,7 +721,14 @@ static char *alloc(int size)
 ******************************************************************************/
 static int dealloc(TRANSAC transac, char *p_buf)
 {
-    GKI_freebuf(transac);
+    if (btif_is_shutdown() == FALSE)
+    {
+        GKI_freebuf(transac);
+    }
+    else
+    {
+        APPL_TRACE_WARNING0("GKI is already shutdown ");
+    }
     return BT_HC_STATUS_SUCCESS;
 }
 